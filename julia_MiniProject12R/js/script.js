ca2a44787e5f36546c1d50925e37b7bb37386c62
julia2021 <jz4934@nyu.edu>
2021-11-27 15:23:57 -0600

    first push

---
+++ julia_MiniProject12R/js/script.js
@@ -0,0 +1,25 @@
+let img;
+function setup() {
+  createCanvas(715, 2900);
+  background(0);
+  img = loadImage("assets/eclipse.jpg");
+}
+function draw() {
+  img.loadPixels();
+
+  for (let i = 0; i < 50; i++) {
+    let x = random(width);
+    let y = random(height);
+    let side = random(5, 25);
+
+    let clr = img.get(x, y);
+
+    let r = red(clr) * 1.0;
+    let g = green(clr) * 1.0;
+    let b = blue(clr) * 1.0;
+    fill(r, g, b);
+    noStroke();
+    square(x, y, side);
+  }
+  img.updatePixels();
+}
